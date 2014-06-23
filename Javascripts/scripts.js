79d22b9af20d7c1f43c85cee3831c0f19e1432c9
aziz-ka <hasanov.aziz@gmail.com>
2014-06-23 00:05:33 -0400

    added js to highlight active menu item

--- Javascripts/scripts.js
+++ Javascripts/scripts.js
@@ -1,4 +1,12 @@
 $(function() {
+	var url = window.location.href;
+	$("header ul li a").each(function() {
+		if(url == this.href) {
+			$(this).addClass("current");
+		}
+	});
+	
+	//	portfolio gallery
 	var hoverme = $(".gallery li");
 	hoverme.hover(function() {
 		$(hoverme).css("-webkit-filter", "blur(3px)");
