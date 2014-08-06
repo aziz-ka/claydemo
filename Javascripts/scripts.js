2d044b4370e69787f6bfc8b5bba2399f6e086722
aziz-ka <hasanov.aziz@gmail.com>
2014-08-05 23:59:51 -0400

    added off canvas navigation

--- Javascripts/scripts.js
+++ Javascripts/scripts.js
@@ -6,13 +6,52 @@ $(function() {
 		}
 	});
 	
-	//	portfolio gallery
-	var hoverme = $(".gallery li");
-	hoverme.hover(function() {
-		$(hoverme).css("-webkit-filter", "blur(3px)");
-		$(this).css("-webkit-filter", "blur(0)");
-	}, function() {
-		$(hoverme).css("-webkit-filter", "blur(0)");
-		$(this).css("-webkit-filter", "blur(0)");
-	});
+	var hoverme = $(".gallery li img");
+	// hoverme.hover(function() {
+	// 	$(hoverme).css("-webkit-filter", "blur(3px)");
+	// 	$(this).css("-webkit-filter", "blur(0)");
+	// }, function() {
+	// 	$(hoverme).css("-webkit-filter", "blur(0)");
+	// 	$(this).css("-webkit-filter", "blur(0)");
+	// });
+	hoverme.click(function() {
+		that = this;
+		var imageURI = $(this).attr("src");
+		$(".largeImage").attr({src: imageURI}).addClass("modal");
+		$(".modal, .overlay").css("visibility", "visible");
+	})
+
+	// var numOfClicks = 0;
+	// $(".next").click(function() {
+	// 	numOfClicks += 1;
+	// 	console.log(numOfClicks);
+	// 	return numOfClicks;
+	// })
+
+	$(".next").click(function() {
+		// debugger
+		var count = hoverme.index(that);
+		if (count >= 0) {
+			$(".largeImage").attr("src", $(hoverme[count + 1]).attr('src'));
+		}
+		if (count === hoverme.length - 1) {
+			$(".largeImage").attr("src", $(hoverme[0]).attr('src'));
+		}
+		console.log(that);
+	})
+	$(".overlay, .close").click(function() {
+		$(hoverme).removeClass("modal");
+		$(".modal, .overlay").css("visibility", "hidden");
+	})
+	
+	$(".toggle").click(function(e) {
+		e.preventDefault();
+		if ($("html").hasClass("openNav")) {
+			$("html").removeClass("openNav");
+			$(".toggle").css("color", "inherit");
+		} else {
+  		$("html").addClass("openNav");
+			$(".toggle").css("color", "#444");
+		}
+	})
 })
